c5deefe4b444f8a29932f3fc338f4784129fdb2e
Americo Meneses <beatamez@hotmail.com>
2021-12-09 19:01:13 -0600

    Initial Commit

---
+++ SuperHeroApiOnMvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperHeroApiOnMvvm
+//
+//  Created by Américo MQ on 09/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
